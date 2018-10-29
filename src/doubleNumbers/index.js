254275373c7c07fc0bf8c34a0f8f20d9b8533649
roman <roman.kushytskyy@gmail.com>
2018-10-30 00:06:57 +0200

    fixed tests

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,11 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const numString = `${num}`;
+  const symbolsLength = numString.length;
+
+  if (numString.substring(0, symbolsLength / 2) === numString.substring(symbolsLength / 2)) {
+    return num;
+  }
+
+  return num * 2;
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  const sumNumbers = number =>
+    number
+      .split('')
+      .reduce((prev, current) => parseInt(prev) + parseInt(current));
+
+  return arr.sort((a, b) => sumNumbers(a) > sumNumbers(b));
 };
